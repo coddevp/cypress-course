185d68c31cf8df705788743e9d65db92e482118f
coddevp <coddevp>
2025-07-13 21:34:52 +0200

    New content

---
+++ cypress/e2e/fundamentals.cy.js
@@ -0,0 +1,7 @@
+describe('Fundamentals test', () => {
+  	it('passes', () => {
+    	cy.visit('http://localhost:3000/fundamentals')
+		cy.get('[data-test="fundamentals-header"').contains(/Testing Fundamentals/i)
+		cy.get('[data-test="fundamentals-header"').should('contain.text', 'Testing Fundamentals')
+  	})
+})
