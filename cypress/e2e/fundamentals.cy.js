bda2f6e2df00a42db45dcd93a15d380070d18e78
coddevp <coddevp>
2025-07-14 13:37:44 +0200

    New content

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,7 +1,19 @@
 describe('Fundamentals test', () => {
-  	it('passes', () => {
-    	cy.visit('http://localhost:3000/fundamentals')
+	beforeEach(() => {
+		cy.visit('/fundamentals')
+	})
+
+  	it('Contains correct header text', () => {
 		cy.get('[data-test="fundamentals-header"').contains(/Testing Fundamentals/i)
 		cy.get('[data-test="fundamentals-header"').should('contain.text', 'Testing Fundamentals')
+		cy.getDataTest('fundamentals-header').should('contain.text', 'Testing Fundamentals')
+  	})
+
+	it('Accordion works correctly', () => {
+		cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+		cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+		cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+		cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+		cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
   	})
 })
